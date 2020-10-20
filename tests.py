925639522901c2b6fb15600a7ed33e187f35452c
Marcos Del Sol Vives <marcos@orca.pet>
2020-10-20 02:43:03 +0200

    Added main to tests file

--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
-# Copyright 2019 Marcos Del Sol Vives <marcos@orca.pet>
+# Copyright 2020 Marcos Del Sol Vives <marcos@orca.pet>
 # SPDX-License-Identifier: WTFPL
 
 from collections import namedtuple
 import leekoq
-from unittest import TestCase
+from unittest import TestCase, main
 
 Vector = namedtuple('Vector', ('key', 'plain', 'cipher'))
 
@@ -12,12 +12,14 @@ TEST_VECTORS = [
 	Vector(0xBEEFDEADBEEFDEAD, 0x2000C022, 0x054C90C2)
 ]
 
-class EncryptionTest(TestCase):
-	def test(self):
+class CryptoTest(TestCase):
+	def test_decrypt(self):
+		for vector in TEST_VECTORS:
+			assert leekoq.decrypt(vector.cipher, vector.key) == vector.plain
+
+	def test_encrypt(self):
 		for vector in TEST_VECTORS:
 			assert leekoq.encrypt(vector.plain, vector.key) == vector.cipher
 
-class DecryptionTest(TestCase):
-	def test(self):
-		for vector in TEST_VECTORS:
-			assert leekoq.decrypt(vector.cipher, vector.key) == vector.plain
+if __name__ == '__main__':
+	main()
